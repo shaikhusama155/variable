7fb2b8a99405bedb8ac10e1320db9bd307641e1b
shaikhusama155 <shaikhusama155@gmail.com>
2023-04-11 06:09:09 +0500

    some

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let var1, var2, var3;
+
+let firstName;
+let age;
+let _address;
+let $salary;
+let fullName123;
+
+
+document.write("<h2>Rules for naming JS variables</h2>");
+document.write("<p>Variable names can only contain letters, numbers, underscores (_), and dollar signs ($). For example $my_1stVariable.</p>");
+document.write("<p>Variables must begin with a letter, underscore (_), or dollar sign ($). For example $name, _name, or name.</p>");
+document.write("<p>Variable names are case sensitive.</p>");
+document.write("<p>Variable names should not be JavaScript keywords.</p>");
